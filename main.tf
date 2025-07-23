ee94b31986e348be709da3aa35253499875a19c4
Joseph Thomas <josephdthomas@google.com>
2025-07-23 22:23:11 +0000

    Add module for DC git proxy for a public repo
    Change-Id: I0b6201bc49d22a4045a15fb5f92a527760b03048

---
+++ main.tf
@@ -0,0 +1,11 @@
+module "basic_example" {
+  source = "https://us-central1-git.developerconnect.dev/140625807922/example-repo/josephdt12-example-repo"
+
+  # You can override the default variable value
+  input_value = "This is a custom message."
+}
+
+# You can then use the module's output
+output "message_from_module" {
+  value = module.basic_example.output_value
+}
--- output.tf
+++
@@ -1,6 +0,0 @@
-# This file defines the values that the module will return.
-
-output "output_value" {
-  description = "The value passed into the module."
-  value       = var.input_value
-}
