d855dab6d9fb92e5161977c3906a4f731ce4da7b
Joseph Thomas <josephdthomas@google.com>
2025-07-30 18:14:24 +0000

    Add .git suffix to module source
    Change-Id: I16ef6d3aa750ec196836012d27991f649a0758db

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "basic_example" {
-  source = "https://us-central1-git.developerconnect.dev/140625807922/example-repo/josephdt12-example-repo"
+  source =
+  "https://us-central1-git.developerconnect.dev/140625807922/example-repo/josephdt12-example-repo.git"
 
   # You can override the default variable value
   input_value = "This is a custom message."
